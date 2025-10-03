ed2cd466102ebeb2ad2e6054582f64a23e80d7a4
Ateeq Ur Rehman <ateeqkhalid623@gmail.com>
2025-10-03 04:57:33 -0700

    Add files via upload

---
+++ 05_JavaScript/07_Strings/script1.js
@@ -0,0 +1,4 @@
+let a="Ahmed";
+let b="JavaScript";
+
+console.log(`My name is ${a} and I am learning ${b}`);
---
+++ 05_JavaScript/07_Strings/script2.js
@@ -0,0 +1,11 @@
+let x=" JavaScript ";
+let y= "Ruby";
+console.log(x.toUpperCase());
+console.log(x.toLowerCase());
+console.log(x.trim());
+
+console.log(x.replace("S"," $"));
+
+console.log(x.concat( y," Python" ," C"," Java"));
+
+console.log(x)
