f5ea479def2cafce4826c44bcca10055a79fb6d7
Ateeq Ur Rehman <ateeqkhalid623@gmail.com>
2025-10-08 04:45:24 -0700

    Add files via upload

---
+++ 05_JavaScript/12_DOM/script.js
@@ -0,0 +1,19 @@
+console.log ("Hello i am learning DOM.");
+
+
+/*
+Try it on console.
+
+document.body.childNodes
+document.body.childNodes[1].childNodes
+
+let cont =document.body.childNodes
+cont.firstElementChild
+cont.lastElementChild
+
+cont.lastElementChild.style.color = "red";
+cont.lastElementChild.innerText = "Hello i am learning DOM.";
+cont.lastElementChlid.style.background = "yellow";
+cont.firstElementChild.ParentElement
+
+*/
